376baeb302fefe3da03dcd2399f2617c4ef60619
romeucajamba <romeucajamba@gmail.com>
2026-01-31 15:36:04 +0100

    consumo de cookie para verificar se usuario está autenticado

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,8 +1,10 @@
 import { useAuthStore } from '@/store/authStrore';
 import { Navigate } from 'react-router-dom';
+import { useAuthToken } from '@/lib/cookie';
 
 export function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const { getToken } = useAuthToken();
   
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
